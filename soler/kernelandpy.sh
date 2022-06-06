f848995d10c8d4bbd88fbb2ba2fa9aab98f7ee25
Said Neder <snederc@protonmail.com>
2022-06-06 11:44:33 -0500

    first

---
+++ soler/kernelandpy.sh
@@ -0,0 +1,9 @@
+# Actualizamos el sistema y sus repositorios
+sudo apt update -y
+sudo apt upgrade -y
+
+# Instalamos utilidades
+sudo apt install python python-pip thonny codeblocks neovim ssh broadcom* -y
+
+# Utilizamos PIP para instalar librerias
+pip install tkinter
